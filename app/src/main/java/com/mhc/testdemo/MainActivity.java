7acd200660b73f0c0873419b2b02182c0feaa011
苗慧存 <miaohc@galanz.com>
2019-08-10 09:25:52 +0800

    创建demo

---
+++ app/src/main/java/com/mhc/testdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mhc.testdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
